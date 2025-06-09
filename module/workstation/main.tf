a48e76ae114d7b8f363e261f31fce5c7b81cb078
karthikeyan <karthikeyangopal16@gmail.com>
2025-06-09 18:53:49 +0200

    terraform ec2 workstation

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -12,8 +12,8 @@ resource "aws_instance" "workstation" {
   ami = data.aws_ami.workstation-ami.image_id
   instance_type = var.instance_type
 
-   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id.id
-   vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.sg_id.id
+   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+   vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.sg_id
 
 
 tags = {
