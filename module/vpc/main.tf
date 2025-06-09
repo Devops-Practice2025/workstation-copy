fce4618b41a3bce520e5b3b2db68aabcc7076341
karthikeyan <karthikeyangopal16@gmail.com>
2025-06-09 17:43:13 +0200

    terraform ec2 workstation

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
   tags = {
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "public" {
    vpc_id = aws_vpc.my_vpc.id
- cidr_block = "10.0.1.0/25"
+ cidr_block = "10.0.1.0/24"
  map_public_ip_on_launch = true
  tags = {
  Name = "public-subnet"
