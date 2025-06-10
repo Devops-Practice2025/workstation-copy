6d65e70373bca5c344b5024c5244077545877288
karthikeyan <karthikeyangopal16@gmail.com>
2025-06-10 11:27:54 +0200

    terraform ec2 workstation

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "workstation" {
   instance_type = var.instance_type
 
   subnet_id = var.subnet_id
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
 
 
   tags = {
