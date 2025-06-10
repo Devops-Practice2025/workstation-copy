e2937abc010dc53d793067e1492b0e33c9318adc
karthik-t-g <karthikeyangopal16@gmail.com>
2025-06-10 09:08:31 +0530

    deleted workstation in main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "workstation" {
-  source = "./module/workstation"
-  instance_name = "workstation"
-  instance_type = "t2.medium"
-}
+#
 module "vpc" {
   source = "./module/vpc"
  }
