fa20ba65336bbae56b2860ee0b6833bf80d4cefc
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2025-07-16 12:07:03 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,11 +3,9 @@ variable "tools" {
 
 workstation = {
         instance_type = "t2.medium"
-        instance_name = "workstation
-        }
+                }
     
 github_runner = {
         instance_type = "t2.medium"
-        instance_name = "github_runner"
-        }
+                }
     } }
