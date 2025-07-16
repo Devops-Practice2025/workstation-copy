59725d37baeb0f36938f65d811b2847a7f86bf35
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2025-07-16 11:49:09 +0200

    Update main.tf
    add tools github runner

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ module "vpc" {
   source = "./module/vpc"
 
  }
-module "workstation" {
+module "workstation" {  
+  for_each = var.tools
   source = "./module/workstation"
-  instance_name = "workstation"
-  instance_type = "t2.medium"
+  instance_name = each.key[instance_name]
+  instance_type = each.key[instance_type]
   subnet_id = module.vpc.subnet_id
   sg_id = module.vpc.sg_id
 }
