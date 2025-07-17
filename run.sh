043c3632c97f40ff86fb91be7061352e6bfbd462
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2025-07-17 12:20:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo yum install -y yum-utils shadow-utils\
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo\
-sudo dnf install terraform\
-git clone https://github.com/karthikeyan-g16/workstation-terraform.git\
+sudo yum install -y yum-utils shadow-utils\n
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo\n
+sudo dnf install terraform\n
+git clone https://github.com/karthikeyan-g16/workstation-terraform.git
