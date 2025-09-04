85da93f080be1413683ac91d630f0b78a37d8dbd
karthikeyan <karthikeyangopal16@gmail.com>
2025-09-04 13:53:28 +0200

    kubectl sh

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Login to AWS CLI\
 aws configure\
 aws s3api create-bucket --bucket karthi-bucket2025 --region us-east-1
+
--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo dnf install terraform
+#kubectl run as root
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://dl.k8s.io/release/v1.34.0/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+kubectl version
+
+
+#terraform
+yum install -y yum-utils shadow-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+dnf install terraform
 git clone https://github.com/karthikeyan-g16/workstation-terraform.git
