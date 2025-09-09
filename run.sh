ed406c687ac71dc4c6d970ccf9b16510cb2e3ac8
karthik-t-g <karthikeyangopal16@gmail.com>
2025-09-10 04:44:01 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,27 @@
+#docker
+sudo dnf update -y
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world 
+sudo usermod -aG docker ec2-user
+sudo docker run hello-world
+docker --version
+
+
 #kubectl run as root
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO https://dl.k8s.io/release/v1.34.0/bin/linux/amd64/kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
-kubectl version
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo kubectl version --client
+sudo kubectl version
 
 
 #terraform
-yum install -y yum-utils shadow-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-dnf install terraform
-git clone https://github.com/karthikeyan-g16/workstation-terraform.git
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo dnf install terraform
+
