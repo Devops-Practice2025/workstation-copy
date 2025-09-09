2a642f2b74546ef72d9d8099273cae9f7bcf49eb
karthikeyan <karthikeyangopal16@gmail.com>
2025-09-09 08:55:56 +0200

    increase volume

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -5,6 +5,13 @@ resource "aws_instance" "workstation" {
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
 
+  root_block_device = {
+    volume_size = 50
+    volume_type = "gp3"      #increase volume to 50gb
+    delete_on_termination = true
+
+  }
+
 
   tags = {
     Name = var.instance_name
--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "tools" {
   default = {
     workstation = {
-      instance_type = "t2.medium"
+      instance_type = "t2.large"
     }
     app_node_1 = {
       instance_type = "t2.medium"
