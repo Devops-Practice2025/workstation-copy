9e6297b5fea2df8566ec88c753a816a5b16892fc
karthikeyan <karthikeyangopal16@gmail.com>
2025-11-10 10:24:28 +0100

    test

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "workstation" {
 
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
-
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   root_block_device {
     volume_size = 50
     volume_type = "gp3"      #increase volume to 50gb
@@ -13,8 +13,59 @@ resource "aws_instance" "workstation" {
   }
 
 
+
   tags = {
     Name = var.instance_name
   }
 } 
 
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-managed-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess" # Example policy
+}
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+
+resource "null_resource" "run_ansible_playbook" {
+  depends_on = [
+    aws_instance.workstation,
+    aws_iam_role_policy_attachment.ec2_policy_attachment  # Replace with your actual resource name
+  ]
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Hello from Terraform!",
+      "sudo dnf install ansible",
+      "git clone https://github.com/Devops-Practice2025/workstation-copy.git",
+      "cd ~/workstation-copy/ansible",
+      "ansible-playbook -i localhost, -e ansible_user=ec2-user -e ansible_password=DevOps321 main.yml -v"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"               # or "ubuntu"
+      password = "DevOps321"          # ⚠️ Not secure
+      host     = self.public_ip
+    }
+  }
+
+
+  }
