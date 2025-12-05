19c395e08e17d4abe9ea57978dd1fe2f76974cae
karthikeyan <karthikeyangopal16@gmail.com>
2025-12-05 15:03:03 +0100

    test

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "tool" {
   vpc_security_group_ids = [var.sg_id]
 
   root_block_device {
-    volume_size = 50
+    volume_size = 70
     volume_type = "gp3"      #increase volume to 50gb
     delete_on_termination = true
 
